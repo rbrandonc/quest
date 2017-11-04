631d67c8c796b7de4fe1a88aa8bcf1699b458744
Nathan Schwartz <cgtnathan@gmail.com>
2017-11-04 15:31:13 -0400

    All the users

--- fetch-server/routes/user.ts
+++ fetch-server/routes/user.ts
@@ -1,28 +1,24 @@
 module.exports = {
-  // getUser: app.get("/api/user", function(req , res){
-  //   var query = "select * from [user]";
-  //   executeQuery (res, query);
-  // }),
-  //
-  // //POST API
-  //  updateUser: app.post("/api/user ", function(req , res){
-  //   var query = "INSERT INTO [user] (Name,Email,Password) VALUES (req.body.Name,req.body.Email,req.body.Password)";
-  //   executeQuery (res, query);
-  // }),
-  //
-  // //PUT API
-  //  createUser: app.put("/api/user/:id", function(req , res){
-  //   var query = "UPDATE [user] SET Name= " + req.body.Name  +  " , Email=  " + req.body.Email + "  WHERE Id= " + req.params.id;
-  //   executeQuery (res, query);
-  // }),
-  //
-  // // DELETE API
-  //  deleteUser: app.delete("/api/user /:id", function(req , res){
-  //   var query = "DELETE FROM [user] WHERE Id=" + req.params.id;
-  //   executeQuery (res, query);
-  // })
   getUser: (req, res) => {
     var query = "select * from user";
     executeQuery (res, query);
+  },
+
+  //POST API
+  updateUser: (req, res) => {
+    var query = "INSERT INTO user (Name,Email,Password) VALUES (req.body.Name,req.body.Email,req.body.Password)";
+    executeQuery (res, query);
+  },
+
+  //PUT API
+  createUser: (req, res) => {
+    var query = "select * from user";
+    executeQuery (res, query);
+  },
+
+  //DELETE API
+  deleteUser: (req, res) => {
+  var query = "DELETE FROM user WHERE Id=" + req.params.id;
+  executeQuery (res, query);
   }
 }
