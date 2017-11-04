46cf0d7fb27533b50f170a6f54d2c07c1542bec5
rbrandonc <r.brandon.c@gmail.com>
2017-11-04 14:25:57 -0400

    fix

---
+++ fetch-server/build/routes/user.js
@@ -0,0 +1,22 @@
+module.exports = {
+    getUser: app.get("/api/user ", function (req, res) {
+        var query = "select * from [user]";
+        executeQuery(res, query);
+    }),
+    //POST API
+    updateUser: app.post("/api/user ", function (req, res) {
+        var query = "INSERT INTO [user] (Name,Email,Password) VALUES (req.body.Name,req.body.Email,req.body.Password)";
+        executeQuery(res, query);
+    }),
+    //PUT API
+    createUser: app.put("/api/user/:id", function (req, res) {
+        var query = "UPDATE [user] SET Name= " + req.body.Name + " , Email=  " + req.body.Email + "  WHERE Id= " + req.params.id;
+        executeQuery(res, query);
+    }),
+    // DELETE API
+    deleteUser: app.delete("/api/user /:id", function (req, res) {
+        var query = "DELETE FROM [user] WHERE Id=" + req.params.id;
+        executeQuery(res, query);
+    }),
+};
+//# sourceMappingURL=user.js.map
---
+++ fetch-server/build/server.js
@@ -0,0 +1,53 @@
+//Initiallising node modules
+var express = require("express");
+var bodyParser = require("body-parser");
+var sql = require("mssql");
+var app = express();
+// Setting Base directory
+app.use(bodyParser.json());
+//CORS Middleware
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, contentType,Content-Type, Accept, Authorization");
+    next();
+});
+//Setting up server
+var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+});
+//Initiallising connection string
+var dbConfig = {
+    user: "<dbUserName>",
+    password: "<dbPassword>",
+    server: "<dbHost_URL>",
+    database: "<dbName>"
+};
+//Function to connect to database and execute query
+var executeQuery = function (res, query) {
+    sql.connect(dbConfig, function (err) {
+        if (err) {
+            console.log("Error while connecting database :- " + err);
+            res.send(err);
+        }
+        else {
+            // create Request object
+            var request = new sql.Request();
+            // query to the database
+            request.query(query, function (err, res) {
+                if (err) {
+                    console.log("Error while querying database :- " + err);
+                    res.send(err);
+                }
+                else {
+                    res.send(res);
+                }
+            });
+        }
+    });
+};
+var routes = require('./routes');
+app.use('/api', routes);
+//# sourceMappingURL=server.js.map
