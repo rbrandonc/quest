efbcd1f9edf7946cc049ee4d48def13a7e451469
rbrandonc <r.brandon.c@gmail.com>
2017-11-04 20:38:39 -0400

    asdf

--- quests-server/build/create_schema.js
+++
@@ -1,9 +0,0 @@
-var db = require('./build/db');
-require('require-sql');
-var queries = require('./schema.sql');
-var res = '';
-for (let query of queries.split(';')) {
-    db.query(res, query);
-    console.log(query);
-}
-//# sourceMappingURL=create_schema.js.map
